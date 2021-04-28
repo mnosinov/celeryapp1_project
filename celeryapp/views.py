e2607f772c307520810473658b7c027fc2ca45d6
mnosinov <mnosinov@gmail.com>
2021-04-28 17:24:34 +0600

    ready to use

--- celeryapp/views.py
+++ celeryapp/views.py
@@ -8,5 +8,5 @@ class IndexView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
-        hello_world().delay()
+        hello_world.delay()
         return context
